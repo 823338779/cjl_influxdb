fa6f7fd24e521e560fd0da7c1eae71cb7b98d801
Caijialun <823338779@qq.com>
2024-04-27 16:47:42 +0800

    实时时间

--- cjl/query_db.sh
+++ cjl/query_db.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 curl -G "http://localhost:8086/query?pretty=true" --data-urlencode "db=mydb" \
---data-urlencode "q=SELECT * FROM cpu WHERE host='server03' AND time < now() - 1d"
+--data-urlencode "q=SELECT * FROM cpu WHERE host='server03'"
--- cjl/write_point.sh
+++ cjl/write_point.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+timestamp=$(date +%s%N)
+curl -XPOST "http://localhost:8086/write?db=mydb" \
+-d 'cpu,host=server01,region=uswest load=42 '"${timestamp}"
 
 curl -XPOST "http://localhost:8086/write?db=mydb" \
--d 'cpu,host=server01,region=uswest load=42 1434055562000000000'
+-d 'cpu,host=server02,region=uswest load=78 '"${timestamp}"
 
 curl -XPOST "http://localhost:8086/write?db=mydb" \
--d 'cpu,host=server02,region=uswest load=78 1434055562000000000'
+-d 'cpu,host=server03,region=useast load=15.4 '"${timestamp}"
 
 curl -XPOST "http://localhost:8086/write?db=mydb" \
--d 'cpu,host=server03,region=useast load=15.4 1434055562000000000'
+-d 'cpu,host=server03,region=useast load=66.6 '"${timestamp}"
